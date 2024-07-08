5c73e380e278f8ec6eeb1bbc33dec676a6ac3feb
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-07-08 10:45:02 +0200

    Initial Commit

---
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Łukasz Andrzejewski on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
